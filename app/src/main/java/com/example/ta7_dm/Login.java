e6dfc55d130cffeedd76f70ced9ef5c9bf692d6a
DarienBustos <dardbust@espol.edu.ec>
2021-12-20 20:02:49 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/ta7_dm/Login.java
@@ -0,0 +1,52 @@
+package com.example.ta7_dm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.amst8.USUARIO";
+    private EditText usn, pw;
+    private SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        usn = findViewById(R.id.editTextName);
+        pw = findViewById(R.id.editTextPassword);
+
+        this.deleteDatabase("bookstore");
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "bookstore", null, 1);
+        db = admin.getReadableDatabase();
+    }
+
+    public void ingresar(View view){
+        String u = usn.getText().toString().trim();
+        String p = pw.getText().toString();
+        if(dbHasData(u, p)){
+            db.close();
+            Intent i = new Intent(this, Main.class);
+            i.putExtra(EXTRA_MESSAGE, u);
+            startActivity(i);
+            finish();
+        }else{
+            Toast toast = Toast.makeText(this, "Contraseña incorrecta o Usuario no existente", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public boolean dbHasData(String searchKey, String searchKey2){
+        String query = "Select * from userdata where username = ? and password = ?";
+        return db.rawQuery(query, new String[]{searchKey, searchKey2}).moveToFirst();
+    }
+
+    public void salirApp(View view){
+        finishAffinity();
+    }
+}
---
+++ app/src/main/java/com/example/ta7_dm/Main.java
@@ -0,0 +1,157 @@
+package com.example.ta7_dm;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main extends AppCompatActivity {
+    private LinearLayout container;
+    private EditText et;
+    private View showdescLayout;
+    private AlertDialog showDesc;
+    private SQLiteDatabase db;
+    private String select_query = "select * from libro";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et = findViewById(R.id.et);
+        container = findViewById(R.id.containerBooks);
+        showdescLayout = LayoutInflater.from(this).inflate(R.layout.custom_desc, null);
+        showDesc = new AlertDialog.Builder(this).create();
+        showDesc.setView(showdescLayout);
+
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "bookstore", null, 1);
+        db = admin.getReadableDatabase();
+
+        String inicial = select_query;
+        Intent cond = getIntent();
+        if(cond.hasExtra(Categorias.EXTRA_MESSAGE)){
+            inicial = select_query.concat("where idcategoria =").concat(cond.getStringExtra(Categorias.EXTRA_MESSAGE));
+        }
+
+        crearLista(inicial);
+        buscar();
+    }
+
+    public void crearLista(final String query){
+        new Thread(new Runnable(){
+
+            @Override
+            public void run(){
+                Cursor fila = db.rawQuery(query, null);
+                if(fila.moveToFirst()) {
+                    do{
+                        final LinearLayout row = new LinearLayout(getApplicationContext());
+                        row.setOrientation(LinearLayout.HORIZONTAL);
+                        row.setLayoutDirection(LinearLayout.LAYOUT_DIRECTION_LTR);
+                        row.setGravity(Gravity.CENTER_VERTICAL);
+
+                        ImageView toShow = new ImageView(getApplicationContext());
+                        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(168,168);
+                        toShow.setLayoutParams(lp);
+                        toShow.setPadding(8,8,8,8);
+                        String src = fila.getString(5);
+                        toShow.setImageResource(getResources().getIdentifier(src,"mipmap", getPackageName()));
+                        row.addView(toShow);
+
+                        LinearLayout list = new LinearLayout(getApplicationContext());
+                        list.setOrientation(LinearLayout.VERTICAL);
+
+                        String[] args = new String[]{"TITULO: ", "AUTOR: ", "AÑO DE UBLICACION: "};
+                        for(int x=1; x<4; x++){
+                            TextView l=new TextView(getApplicationContext());
+                            l.setTextSize(16);
+                            l.setTextColor(Color.LTGRAY);
+                            String data = fila.getString(x);
+                            if(fila.getString(x).isEmpty()){
+                                data = "Desconocido";
+                            }
+                            l.setText(args[x-1].concat(data));
+                            list.addView(l);
+                        }
+                        row.addView(list);
+
+                        final String desc = fila.getString(1).concat("\n").concat(fila.getString(4));
+                        container.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                container.addView(row);
+                                row.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        mostrarDesc(desc);
+                                    }
+                                });
+                            }
+                        });
+                    }while (fila.moveToNext());
+                }
+                fila.close();
+            }
+        }).start();
+    }
+
+    public void buscar(){
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                container.removeAllViews();
+                crearLista(select_query.concat("where titulo like '").concat(s.toString()).concat("%'"));
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    public void mostrarDesc(String data){
+        String[] args = data.split("\n");
+        TextView title = showdescLayout.findViewById(R.id.text_title);
+        title.setText(args[0]);
+        TextView desc = showdescLayout.findViewById(R.id.text_dialog);
+        title.setText(args[1]);
+        showDesc.show();
+    }
+
+    public void verPerfil(View view){
+        Intent i= new Intent(this, Perfil_Usuario.class);
+        Bundle datos = this.getIntent().getExtras();
+        String msgl = datos.getString(Login.EXTRA_MESSAGE);
+        startActivity(i);
+    }
+
+    public void verCategorias(View view){
+        Intent i = new Intent(this, Categorias.class);
+        Bundle datos = this.getIntent().getExtras();
+        String msgl = datos.getString(Login.EXTRA_MESSAGE);
+        i.putExtra(Login.EXTRA_MESSAGE,msgl);
+        startActivity(i);
+    }
+
+    public void inicio(View view){
+        Toast.makeText(this, "Se encuentra en la seccion principal", Toast.LENGTH_SHORT).show();
+    }
+}
